ebc24f3f88705fd62f6be91dcfc1bf945946582c
Stuart Reid <stuart1334@gmail.com>
2021-04-02 15:43:52 +0100

    Add project files.

---
+++ CraftingInterpreters/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CraftingInterpreters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
