585aa1a4b9e26243330bfd5315e8ad79f8f0502e
Stuart Reid <stuart1334@gmail.com>
2021-04-03 22:23:47 +0100

    Add token class

---
+++ LoxDotNet/Scanner/Token.cs
@@ -0,0 +1,23 @@
+﻿namespace LoxDotNet.Scanner
+{
+    class Token
+    {
+        internal TokenType Type;
+        internal string Lexeme;
+        internal object Literal;
+        internal int Line;
+
+        internal Token(TokenType type, string lexeme, object literal, int line)
+        {
+            Type = type;
+            Lexeme = lexeme;
+            Literal = literal;
+            Line = line;
+        }
+
+        public override string ToString()
+        {
+            return $"{Type} {Lexeme} {Literal}";
+        }
+    }
+}
