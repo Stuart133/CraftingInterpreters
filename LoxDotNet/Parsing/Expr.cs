a468ce0d53b02d90581d6720f1b1b7ba6e66edba
Stuart Reid <stuart1334@gmail.com>
2021-04-06 08:34:30 +0100

    Add visitor pattern to AST generator

--- LoxDotNet/Parsing/Expr.cs
+++ LoxDotNet/Parsing/Expr.cs
@@ -1,10 +1,20 @@
+// This file is auto generated from LoxTools - Do not modify directly
+
 using LoxDotNet.Scanning;
 
 namespace LoxDotNet.Parsing
 {
 	abstract class Expr
 	{
-		class Binary : Expr
+		internal interface IVisitor<T>
+		{
+			internal T VisitBinaryExpr(Binary expr);
+			internal T VisitGroupingExpr(Grouping expr);
+			internal T VisitLiteralExpr(Literal expr);
+			internal T VisitUnaryExpr(Unary expr);
+		}
+
+		internal class Binary : Expr
 		{
 			internal Binary(Expr left, Token op, Expr right)
 			{
@@ -13,32 +23,47 @@ namespace LoxDotNet.Parsing
 				this.right = right;
 			}
 
+			internal override T Accept<T>(IVisitor<T> visitor)
+			{
+				return visitor.VisitBinaryExpr(this);
+			}
+
 			internal Expr left { get; }
 			internal Token op { get; }
 			internal Expr right { get; }
 		}
 
-		class Grouping : Expr
+		internal class Grouping : Expr
 		{
 			internal Grouping(Expr expression)
 			{
 				this.expression = expression;
 			}
 
+			internal override T Accept<T>(IVisitor<T> visitor)
+			{
+				return visitor.VisitGroupingExpr(this);
+			}
+
 			internal Expr expression { get; }
 		}
 
-		class Literal : Expr
+		internal class Literal : Expr
 		{
 			internal Literal(object value)
 			{
 				this.value = value;
 			}
 
+			internal override T Accept<T>(IVisitor<T> visitor)
+			{
+				return visitor.VisitLiteralExpr(this);
+			}
+
 			internal object value { get; }
 		}
 
-		class Unary : Expr
+		internal class Unary : Expr
 		{
 			internal Unary(Token op, Expr right)
 			{
@@ -46,9 +71,15 @@ namespace LoxDotNet.Parsing
 				this.right = right;
 			}
 
+			internal override T Accept<T>(IVisitor<T> visitor)
+			{
+				return visitor.VisitUnaryExpr(this);
+			}
+
 			internal Token op { get; }
 			internal Expr right { get; }
 		}
 
+		internal abstract T Accept<T>(IVisitor<T> visitor);
 	}
 }
--- LoxTools/GenerateAst.cs
+++ LoxTools/GenerateAst.cs
@@ -30,54 +30,85 @@ namespace LoxTools
         private static void DefineAst(string outputDir, string baseName, IEnumerable<string> types)
         {
             var path = $"{outputDir}/{baseName}.cs";
-            var stringBuilder = new StringBuilder();
+            var builder = new StringBuilder();
 
-            stringBuilder.AppendLine("using LoxDotNet.Scanning;");
-            stringBuilder.AppendLine();
-            stringBuilder.AppendLine("namespace LoxDotNet.Parsing");
-            stringBuilder.AppendLine("{");
-            stringBuilder.AppendLine($"\tabstract class {baseName}");
-            stringBuilder.AppendLine("\t{");
+            builder.AppendLine("// This file is auto generated from LoxTools - Do not modify directly");
+            builder.AppendLine();
+
+            builder.AppendLine("using LoxDotNet.Scanning;");
+            builder.AppendLine();
+            builder.AppendLine("namespace LoxDotNet.Parsing");
+            builder.AppendLine("{");
+            builder.AppendLine($"\tabstract class {baseName}");
+            builder.AppendLine("\t{");
+
+            // Create the visitor interface
+            DefineVisitor(builder, baseName, types);
+            builder.AppendLine();
 
             // Create the sub classes
             foreach (var type in types)
             {
                 var className = type.Split(':')[0].Trim();
                 var fields = type.Split(':')[1].Trim();
-                DefineType(stringBuilder, baseName, className, fields);
+                DefineType(builder, baseName, className, fields);
             }
 
-            stringBuilder.AppendLine("\t}");
-            stringBuilder.AppendLine("}");
-            File.WriteAllText(path, stringBuilder.ToString());
+            // Add the base Accept<T> method
+            builder.AppendLine("\t\tinternal abstract T Accept<T>(IVisitor<T> visitor);");
+
+            builder.AppendLine("\t}");
+            builder.AppendLine("}");
+            File.WriteAllText(path, builder.ToString());
         }
 
-        private static void DefineType(StringBuilder stringBuilder, string baseName, string className, string fieldList)
+        private static void DefineVisitor(StringBuilder builder, string baseName, IEnumerable<string> types)
         {
-            stringBuilder.AppendLine($"\t\tclass {className} : {baseName}");
-            stringBuilder.AppendLine("\t\t{");
+            builder.AppendLine("\t\tinternal interface IVisitor<T>");
+            builder.AppendLine("\t\t{");
+
+            foreach (var type in types)
+            {
+                var typeName = type.Split(':')[0].Trim();
+                builder.AppendLine($"\t\t\tinternal T Visit{typeName}{baseName}({typeName} {baseName.ToLower()});");
+            }
+
+            builder.AppendLine("\t\t}");
+        }
+
+        private static void DefineType(StringBuilder builder, string baseName, string className, string fieldList)
+        {
+            builder.AppendLine($"\t\tinternal class {className} : {baseName}");
+            builder.AppendLine("\t\t{");
 
             // Constructor
-            stringBuilder.AppendLine($"\t\t\tinternal {className}({fieldList})");
-            stringBuilder.AppendLine("\t\t\t{");
+            builder.AppendLine($"\t\t\tinternal {className}({fieldList})");
+            builder.AppendLine("\t\t\t{");
 
             // Store parameters in properties
             foreach (var field in fieldList.Split(", "))
             {
                 var name = field.Split(' ')[1];
-                stringBuilder.AppendLine($"\t\t\t\tthis.{name} = {name};");
+                builder.AppendLine($"\t\t\t\tthis.{name} = {name};");
             }
-            stringBuilder.AppendLine("\t\t\t}");
-            stringBuilder.AppendLine();
+            builder.AppendLine("\t\t\t}");
+            builder.AppendLine();
+
+            // Visitor implementation
+            builder.AppendLine("\t\t\tinternal override T Accept<T>(IVisitor<T> visitor)");
+            builder.AppendLine("\t\t\t{");
+            builder.AppendLine($"\t\t\t\treturn visitor.Visit{className}{baseName}(this);");
+            builder.AppendLine("\t\t\t}");
+            builder.AppendLine();
 
             // Properties
             foreach (var field in fieldList.Split(", "))
             {
-                stringBuilder.AppendLine($"\t\t\tinternal {field} {{ get; }}");
+                builder.AppendLine($"\t\t\tinternal {field} {{ get; }}");
             }
 
-            stringBuilder.AppendLine("\t\t}");
-            stringBuilder.AppendLine();
+            builder.AppendLine("\t\t}");
+            builder.AppendLine();
         }
     }
 }
