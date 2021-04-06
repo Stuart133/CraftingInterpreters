84a10aab0d1839bd335ee2feecfae1b96a798114
Stuart Reid <stuart1334@gmail.com>
2021-04-06 22:04:11 +0100

    Add the start of the parser

---
+++ LoxDotNet/Parsing/Parser.cs
@@ -0,0 +1,99 @@
+﻿using LoxDotNet.Scanning;
+using System.Collections.Generic;
+using static LoxDotNet.Scanning.TokenType;
+
+namespace LoxDotNet.Parsing
+{
+    class Parser
+    {
+        private readonly List<Token> _tokens;
+        private int _current = 0;
+
+        internal Parser(List<Token> tokens)
+        {
+            _tokens = tokens;
+        }
+
+        private bool Match(params TokenType[] types)
+        {
+            foreach (var tokenType in types)
+            {
+                if (Check(tokenType))
+                {
+                    Advance();
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool Check(TokenType type)
+        {
+            if (IsAtEnd())
+            {
+                return false;
+            }
+
+            return Peek().Type == type;
+        }
+
+        private Token Advance()
+        {
+            if (!IsAtEnd())
+            {
+                _current++;
+            }
+
+            return Previous();
+        }
+
+        private Token Peek()
+        {
+            return _tokens[_current];
+        }
+
+        private Token Previous()
+        {
+            return _tokens[_current - 1];
+        }
+
+        private bool IsAtEnd()
+        {
+            return Peek().Type == EOF;
+        }
+
+        private Expr Expression()
+        {
+            return Equality();
+        }
+
+        private Expr Equality()
+        {
+            var expr = Comparison();
+
+            while (Match(BANG_EQUAL, EQUAL_EQUAL))
+            {
+                var op = Previous();
+                var right = Comparison();
+                expr = new Expr.Binary(expr, op, right);
+            }
+
+            return expr;
+        }
+
+        private Expr Comparison()
+        {
+            var expr = Term();
+
+            while (Match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL))
+            {
+                var op = Previous();
+                var right = Term();
+                expr = new Expr.Binary(expr, op, right);
+            }
+
+            return expr;
+        }
+    }
+}
